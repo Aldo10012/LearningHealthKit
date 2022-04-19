64f5db02086df2489b6eb08dbdf206d161c87fa9
Alberto <aldo10012@gmail.com>
2022-04-19 14:17:37 -0700

    first commit

---
+++ LearningHealthKit/App/LearningHealthKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningHealthKitApp.swift
+//  LearningHealthKit
+//
+//  Created by Alberto Dominguez on 4/19/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningHealthKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
